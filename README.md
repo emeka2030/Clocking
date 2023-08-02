1811ab8badbd888beccad8e4a67580f8fdd2fa89
MekkyMachine <93871591+emeka2030@users.noreply.github.com>
2023-08-02 18:05:45 -0400

    Progress on script and html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clocking
 Let make a banging clock.
+
+This is my first project, a digital clock.
+
+Finally learned how to commit to git hub. ;/ boy that took some work. Let's get on working on this clock.
--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 //this is where the script will go
+setInterval(showTime, 1000);
+
+function showTime(){
+
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let sec = time.getSeconds();
+    amorpm = "am";
+
+    if(hours >= 12){
+        amorpm = "pm";
+    }else{
+        amorpm = "am";
+    }
+
+   hours = hours < 10 ? "0" + hours : hours;
+   minutes = minutes < 10 ? "0" + minutes : minutes;
+   sec = sec < 10 ? "0" + sec : sec;
+
+   let currentTime = hours + ":" + minutes + ":" + sec + " " + amorpm;
+
+    document.getElementId("clock").innerHTML = currentTime;
+
+}
+
+showTime();
+
+
+
+
+/*function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('state');
+
+    if(hrs >= 12){
+        hrs = hrs - 12;
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+    document.getElementById('state').innerHTML = session;
+}
+setInterval(displayTime, 10);
+
+function displayDayOrNight(){
+
+    if(hrs > 12){
+        let a = "PM";     
+    }else{
+        let a = "AM"
+    }
+    return a;
+}*/
